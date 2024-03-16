fc56af870d578e5491ccb364219d812fd06ec931
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2024-03-16 16:32:16 +0200

    add-img

--- src/js/slider.js
+++ src/js/slider.js
@@ -22,3 +22,31 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+const slider = document.querySelector('.slider');
+const slides = document.querySelector('.slides');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+  updateButtons();
+}
+
+function updateButtons() {
+  prevButton.disabled = currentSlide === 0;
+  nextButton.disabled = currentSlide === slides.children.length - 1;
+}
+
+prevButton.addEventListener('click', () => {
+  currentSlide--;
+  showSlide(currentSlide);
+});
+
+nextButton.addEventListener('click', () => {
+  currentSlide++;
+  showSlide(currentSlide);
+});
+
+showSlide(currentSlide);
