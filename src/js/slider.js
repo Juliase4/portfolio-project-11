492c949da29dab2061e386f59ed2956ea0e2b28b
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2024-03-16 17:21:46 +0200

    fix-js-css

--- src/js/slider.js
+++ src/js/slider.js
@@ -23,29 +23,41 @@ const swiper = new Swiper('.swiper', {
   },
 });
 const slider = document.querySelector('.slider');
-const slides = document.querySelector('.slides');
+const slides = document.querySelectorAll('.slide');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 
 let currentSlide = 0;
 
 function showSlide(slideIndex) {
-  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
   updateButtons();
 }
 
 function updateButtons() {
   prevButton.disabled = currentSlide === 0;
-  nextButton.disabled = currentSlide === slides.children.length - 1;
+  nextButton.disabled = currentSlide === slides.length - 1;
 }
 
 prevButton.addEventListener('click', () => {
   currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
   showSlide(currentSlide);
 });
 
 nextButton.addEventListener('click', () => {
   currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
   showSlide(currentSlide);
 });
 
