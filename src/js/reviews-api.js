ac686986a951b819db53fc6adf0eaf6d4372219e
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-03-18 15:32:19 +0200

    pullback

--- src/js/reviews-api.js
+++ src/js/reviews-api.js
@@ -1,16 +1,8 @@
 import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import iziToast from 'izitoast';
 
 const reviewsList = document.querySelector('.reviews-list');
-const prevButton = document.querySelector(
-  '.reviews-slider-arrow.swiper-button-prev'
-);
-const nextButton = document.querySelector(
-  '.reviews-slider-arrow.swiper-button-next'
-);
+const prevButton = document.querySelector('.rev-swiper-button-prev-js');
+const nextButton = document.querySelector('.rev-swiper-button-next-js');
 
 const API_URL = 'https://portfolio-js.b.goit.study/api/reviews';
 const requestOptions = {
@@ -24,39 +16,6 @@ let itemsPerPage = 1;
 let currentPage = 0;
 let reviews = [];
 
-const swiper = new Swiper('.reviews-swiper', {
-  modules: [Navigation],
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews .swiper-button-next',
-    prevEl: '.reviews .swiper-button-prev',
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-
-  //Кількість слайдів для показу
-  slidesPerView: 1,
-  // slidesPerGroup: 4,
-  speed: 4000,
-
-  breakpoints: {
-    375: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1440: {
-      slidesPerView: 4,
-    },
-  },
-});
-
 function fetchReviews() {
   return fetch(API_URL, requestOptions)
     .then(response => {
@@ -68,19 +27,7 @@ function fetchReviews() {
     .then(data => {
       console.log('Reviews data:', data);
       reviews = data;
-      swiper.navigation.update({
-        breakpoints: {
-          375: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1440: {
-            slidesPerView: 4,
-          },
-        },
-      });
+      updateItemsPerPage();
       renderReviews();
     })
     .catch(error => {
@@ -93,13 +40,9 @@ prevButton.addEventListener('click', () => handlePageChange('prev'));
 
 function handlePageChange(direction) {
   if (direction === 'next') {
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-next',
-    });
+    currentPage++;
   } else {
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-prev',
-    });
+    currentPage--;
   }
   renderReviews();
 }
@@ -114,16 +57,13 @@ function renderReviews() {
   reviewsList.innerHTML = '';
 
   if (reviewsToShow.length === 0) {
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-next',
-      disabled: true,
-    });
+    disableButton(nextButton);
   } else {
     // Рендерити відгуки, які потрібно показати
     reviewsList.innerHTML = reviewsToShow
       .map(
         ({ _id, author, avatar_url, review }) =>
-          `<li class="reviews-item swiper-slide" id='${_id}'>
+          `<li class="reviews-item rev-swiper-slide-js" id='${_id}'>
             <img class="reviews-img" src="${avatar_url}" 
             alt="about-me" width="48" height="48" />
             <h3 class="reviews-name">${author}</h3>
@@ -131,54 +71,49 @@ function renderReviews() {
           </li>`
       )
       .join('');
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-next',
-      disabled: false,
-    });
+    enableButton(nextButton);
   }
 
-  // Перевірка, чи потрібно задізейблити кнопку попереднього слайду
-  if (startIndex === 0) {
-    swiper.navigation.update({
-      prevEl: '.reviews .swiper-button-prev',
-      disabled: true,
-    });
+  if (currentPage === 0) {
+    disableButton(prevButton);
   } else {
-    swiper.navigation.update({
-      prevEl: '.reviews .swiper-button-prev',
-      disabled: false,
-    });
+    enableButton(prevButton);
   }
 
-  // Перевірка, чи потрібно задізейблити кнопку наступного слайду
   if (endIndex >= reviews.length) {
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-next',
-      disabled: true,
-    });
+    disableButton(nextButton);
   } else {
-    swiper.navigation.update({
-      nextEl: '.reviews .swiper-button-next',
-      disabled: false,
-    });
+    enableButton(nextButton);
+  }
+}
+
+// Функція для відключення кнопки
+function disableButton(button) {
+  button.disabled = true;
+  button.classList.add('disabled');
+}
+
+// Функція для включення кнопки
+function enableButton(button) {
+  button.disabled = false;
+  button.classList.remove('disabled');
+}
+
+function updateItemsPerPage() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth < 768) {
+    itemsPerPage = 1;
+  } else if (windowWidth >= 768 && windowWidth < 1439) {
+    itemsPerPage = 2;
+  } else {
+    itemsPerPage = 4;
   }
 }
 
 document.addEventListener('DOMContentLoaded', fetchReviews);
 
 window.addEventListener('resize', () => {
-  swiper.navigation.update({
-    breakpoints: {
-      375: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1440: {
-        slidesPerView: 4,
-      },
-    },
-  });
+  updateItemsPerPage();
   renderReviews();
 });
