8eb091faf164793e2df30b6167568706e6bcdb62
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-03-19 05:38:32 +0200

    swiper

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,10 +3,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
-const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
 
 let currentSlide = 0;
 
@@ -18,34 +15,11 @@ function showSlide(slideIndex) {
       slide.classList.remove('active');
     }
   });
-  updateButtons();
+  initSwiper();
 }
-
-function updateButtons() {
-  prevButton.disabled = currentSlide === 0;
-  nextButton.disabled = currentSlide === slides.length - 1;
-}
-
-prevButton.addEventListener('click', () => {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-  showSlide(currentSlide);
-});
-
-nextButton.addEventListener('click', () => {
-  currentSlide++;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  showSlide(currentSlide);
-});
-
-showSlide(currentSlide);
 
 function initSwiper() {
-  const newSwiper = new Swiper('.reviews-cards', {
+  const proSwiper = new Swiper('.slide', {
     modules: [Navigation],
     // loop: true,
     navigation: {
@@ -57,16 +31,17 @@ function initSwiper() {
     breakpoints: {
       375: {
         slidesPerView: 1,
-        spaceBetween: 16,
       },
       768: {
-        slidesPerView: 2,
-        spaceBetween: 16,
+        slidesPerView: 1,
       },
       1440: {
-        slidesPerView: 4,
-        spaceBetween: 16,
+        slidesPerView: 1,
       },
     },
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  showSlide(currentSlide);
+});
